0aae4098989eede14f3f30c639fd240d015e1ae1
Andrew Keller <Keller192@gmail.com>
2018-05-17 13:49:36 -0400

    initial commit with create-react-app, normalize.css, and a meta description for search engine optimization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Pizza of Italy
+            </div>
+        );
+    }
+}
+
+export default App;
