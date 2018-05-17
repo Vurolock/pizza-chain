32f9e012db037621495b2bccaf6c6d8284f43f52
Andrew Keller <Keller192@gmail.com>
2018-05-17 14:49:44 -0400

    adds and renders location component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Locations from './Locations';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: ''
+        }
+    }
     render() {
         return (
-            <div>
-                
-            </div>
+            <Locations />
         );
     }
 }
---
+++ src/Locations.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Locations = (props) => {
+
+    return (
+        <div className="location-list">
+            <div className="location">
+                Slice of Sicily{'\n'}New York City, NY
+            </div>
+            <div className="location">
+                Neapolitan Nibbles{'\n'}Chicago, IL
+            </div>
+            <div className="location">
+                Rumunating Rome{'\n'}San Francisco, CA
+            </div>
+        </div>
+    );
+}
+
+export default Locations;
