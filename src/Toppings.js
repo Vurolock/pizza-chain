c13c2c54f30647d97ba49765b2769dc143029656
Andrew Keller <Keller192@gmail.com>
2018-05-18 12:11:33 -0400

    adds Toppings component with toppings in a dynamic list

---
+++ src/Toppings.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Toppings = (props) => {
+    
+    let toppings = props.toppings.map((topping, i) => {
+        return (
+            <li
+                key={ i }
+                className="topping"
+            >
+            { topping }
+            </li>
+        );
+    });
+
+    return (
+        <React.Fragment>
+
+            <div className="option-select">
+                { props.toppingsHeading }
+            </div>
+
+            <ul className="toppings-list">
+                { toppings }
+            </ul>
+
+        </React.Fragment>
+    );
+}
+
+export default Toppings;
