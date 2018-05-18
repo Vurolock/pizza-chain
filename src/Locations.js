bf3e2d5ae1164f9645158958af26c1206707d5c1
Andrew Keller <Keller192@gmail.com>
2018-05-18 14:03:55 -0400

    moves franchises array to seperate file

--- src/Locations.js
+++ src/Locations.js
@@ -1,74 +1,8 @@
 import React from 'react';
-
-// Future franchises can be added to this array in order to easily expand the website
-const franchises = [
-    {
-        'name': 'Slice of Sicily',
-        'city': 'New York City, NY',
-        'pizzas': [
-            {
-                'type': 'Cheese',
-                'name': 'Palermo',
-                'toppings': ['No Toppings   ']
-            },
-            {
-                'type': 'Veggie', 
-                'name': 'Marsala',
-                'toppings': ['Mushrooms', 'Artichoke Hearts', 'Red Peppers']
-            },
-            {
-                'type': 'Meat',
-                'name': 'Catania',
-                'toppings': ['Pepperoni', 'Sausage', 'Chicken']
-            }
-        ]
-    },
-    {
-        'name': 'Neapolitan Nibbles',
-        'city': 'Chicago, IL',
-        'pizzas': [
-            {
-                'type': 'Cheese',
-                'name': 'Soccavo',
-                'toppings': ['No Toppings   ']
-            },
-            {
-                'type': 'Veggie', 
-                'name': 'Posillipo',
-                'toppings': ['Mushrooms', 'Olives', 'Basil']
-            },
-            {
-                'type': 'Meat',
-                'name': 'Vomero',
-                'toppings': ['Pepperoni', 'Bacon', 'Salami']
-            }
-        ]
-    },
-    {
-        'name': 'Ruminating Rome',
-        'city': 'San Francisco, CA',
-        'pizzas': [
-            {
-                'type': 'Cheese',
-                'name': 'Labaro',
-                'toppings': ['No Toppings   ']
-            },
-            {
-                'type': 'Veggie',
-                'name': 'Cinecitta',
-                'toppings': ['Mushrooms', 'Broccolini', 'Green Peppers', 'Oregano']
-            },
-            {
-                'type': 'Meat',
-                'name': 'Mostacciano',
-                'toppings': ['Pepperoni', 'Proscuitto', 'Capicola']
-            }
-        ]
-    }
-];
+const franchises = require('./franchises.js');
 
 const Locations = (props) => {
-    // Future franchises can be added to this array in order to easily expand the website
+
     let locations = franchises.map((location, i) => {
         let locationClass = "button";
         if (i === props.location) {
@@ -85,7 +19,8 @@ const Locations = (props) => {
                 <span>({ location.city })</span>
             </div>
         );
-    })
+    });
+    
     return (
         <React.Fragment>
 
---
+++ src/franchises.js
@@ -0,0 +1,68 @@
+// Future franchises can be added to this array in order to easily expand the website
+const franchises = [
+    {
+        'name': 'Slice of Sicily',
+        'city': 'New York City, NY',
+        'pizzas': [
+            {
+                'type': 'Cheese',
+                'name': 'Palermo',
+                'toppings': ['No Toppings   ']
+            },
+            {
+                'type': 'Veggie', 
+                'name': 'Marsala',
+                'toppings': ['Mushrooms', 'Artichoke Hearts', 'Red Peppers']
+            },
+            {
+                'type': 'Meat',
+                'name': 'Catania',
+                'toppings': ['Pepperoni', 'Sausage', 'Chicken']
+            }
+        ]
+    },
+    {
+        'name': 'Neapolitan Nibbles',
+        'city': 'Chicago, IL',
+        'pizzas': [
+            {
+                'type': 'Cheese',
+                'name': 'Soccavo',
+                'toppings': ['No Toppings   ']
+            },
+            {
+                'type': 'Veggie', 
+                'name': 'Posillipo',
+                'toppings': ['Mushrooms', 'Olives', 'Basil']
+            },
+            {
+                'type': 'Meat',
+                'name': 'Vomero',
+                'toppings': ['Pepperoni', 'Bacon', 'Salami']
+            }
+        ]
+    },
+    {
+        'name': 'Ruminating Rome',
+        'city': 'San Francisco, CA',
+        'pizzas': [
+            {
+                'type': 'Cheese',
+                'name': 'Labaro',
+                'toppings': ['No Toppings   ']
+            },
+            {
+                'type': 'Veggie',
+                'name': 'Cinecitta',
+                'toppings': ['Mushrooms', 'Broccolini', 'Green Peppers', 'Oregano']
+            },
+            {
+                'type': 'Meat',
+                'name': 'Mostacciano',
+                'toppings': ['Pepperoni', 'Proscuitto', 'Capicola']
+            }
+        ]
+    }
+];
+
+module.exports = franchises;
